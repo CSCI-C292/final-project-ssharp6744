67a70faed07403e07bd6d62919c7be46f869bf8a
ssharp6744 <70828154+ssharp6744@users.noreply.github.com>
2020-11-24 23:37:04 -0500

    Added Animations

---
+++ Assets/Furniture Extras/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+
+    void OnMouseOver() 
+    {
+        if (Vector3.Distance(GameObject.Find("First Person Player").transform.position, transform.parent.position) <= 2 && Input.GetButtonDown("Fire1"))
+        {
+            _animator.SetTrigger("OpenDoor");
+        }
+    }
+}
---
+++ Assets/Furniture Extras/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField] Transform _destination;
+    [SerializeField] Transform _objectPosition;
+
+    //This video helped me with this script: https://www.youtube.com/watch?v=IEV64CLZra8
+    void OnMouseDown() 
+    {
+        if (Vector3.Distance(GameObject.Find("First Person Player").transform.position, transform.position) <= 2)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().freezeRotation = true;
+            transform.position = _objectPosition.position;
+            transform.parent = GameObject.Find("Destination").transform; 
+        }
+         
+    }
+
+    void OnMouseUp() 
+    {
+        transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true; 
+        GetComponent<Rigidbody>().freezeRotation = false;   
+    }
+}
